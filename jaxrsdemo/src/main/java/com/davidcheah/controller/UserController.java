7d0b9c752b3ecfb83bee586bf082df89322958b4
DavidCheah <tattwei46@gmail.com>
2019-06-28 23:49:53 +0800

    Added GET request with id param

--- jaxrsdemo/src/main/java/com/davidcheah/controller/UserController.java
+++ jaxrsdemo/src/main/java/com/davidcheah/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.davidcheah.model.User;
 import com.davidcheah.services.UserService;
@@ -13,12 +14,18 @@ public class UserController {
 
 	private UserService service = new UserService();
 
-	//http://localhost:8080/jaxrsdemo/rest/service/users
-	//http://localhost:8080/jaxrsdemo/users
+	//http://localhost:8080/jaxrsdemo/service/users
 	@GET
 	@Path("/users")
 	public List<User> getUsers() {
 		List<User> users = service.getAllUsers();
 		return users;
+	}
+	
+	@GET
+	@Path("/users/{id}")
+	public User getUser(@PathParam("id") int id) {
+		User user = service.getUser(id);
+		return user;
 	}
 }
