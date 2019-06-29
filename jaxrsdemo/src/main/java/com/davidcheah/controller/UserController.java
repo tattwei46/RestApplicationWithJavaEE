2737ec2d4be6dd21faa65edd0c88d58ec11fa534
DavidCheah <tattwei46@gmail.com>
2019-06-29 12:50:45 +0800

    Added POST request to add user and empty model constructor

--- jaxrsdemo/src/main/java/com/davidcheah/controller/UserController.java
+++ jaxrsdemo/src/main/java/com/davidcheah/controller/UserController.java
@@ -2,9 +2,13 @@ package com.davidcheah.controller;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.davidcheah.model.User;
 import com.davidcheah.services.UserService;
@@ -27,5 +31,14 @@ public class UserController {
 	public User getUser(@PathParam("id") int id) {
 		User user = service.getUser(id);
 		return user;
+	}
+	
+	@POST
+	@Path("/users")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User addUser(User user) {
+		User newUser= service.add(user);
+		return newUser;
 	}
 }
